0c4a930c49a933a897c310202a3436128de5401e
ccforeverd <zh1045456074@163.com>
2020-04-28 01:11:49 +0800

    add typescript

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,5 +1,3 @@
-'use strict'
-
 import { app, BrowserWindow } from 'electron'
 import * as path from 'path'
 import { format as formatUrl } from 'url'
@@ -7,7 +5,9 @@ import { format as formatUrl } from 'url'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // global reference to mainWindow (necessary to prevent window from being garbage collected)
-let mainWindow
+let mainWindow: any
+
+app.allowRendererProcessReuse = true
 
 function createMainWindow() {
   const window = new BrowserWindow({webPreferences: {nodeIntegration: true}})
